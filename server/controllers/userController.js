9c1b46771015a9014f022a406d8df1448ab70041
Amandeep61 <amandeepsingh70513@gmail.com>
2025-10-31 18:46:34 +0530

    Updated backend username to name

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -4,13 +4,13 @@ import jwt from "jsonwebtoken";
 
 // Register user
 export const registerUser = async (req, res) => {
-  const { username, email, password } = req.body;
+  const { name, email, password } = req.body;
   try {
     const existingUser = await User.findOne({ email });
     if (existingUser) return res.status(400).json({ message: "Email already exists" });
 
     const hashedPassword = await bcrypt.hash(password, 10);
-    await User.create({ username, email, password: hashedPassword });
+    await User.create({ name, email, password: hashedPassword });
 
     res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
@@ -29,7 +29,7 @@ export const loginUser = async (req, res) => {
     if (!isMatch) return res.status(400).json({ message: "Invalid password" });
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: "1d" });
-    res.json({ username: user.username, email: user.email, token });
+    res.json({ name: user.name, email: user.email, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
